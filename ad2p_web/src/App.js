74de535393a848329123ad6a604f3ea0aea24e17
Daniel Lungaro <chunk_77@hotmail.com>
2022-04-07 17:45:48 -0700

    init react app

---
+++ ad2p_web/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>
+         AD-2P
+       </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
